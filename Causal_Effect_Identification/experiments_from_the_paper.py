9b7325ed0940efa64890070839e49e7481cc85ea
Daniele Tramontano <88477195+danieletramontano@users.noreply.github.com>
2025-08-06 11:32:43 +0200

    update identification code

--- Causal_Effect_Identification/experiments_from_the_paper.py
+++ Causal_Effect_Identification/experiments_from_the_paper.py
@@ -5,51 +5,50 @@ Created on Wed May 15 18:56:33 2024
 
 @author: daniele
 """
-
-from identification import parallel_ranggraph, rangraphsimul, count_data, prod_plot_mult_plots, save_object, load_object
 import random
+import identification
 
 
 
-run_par = True # set to False if don't want to parallelize
-if run_par is True:
+PARALLELIZE = True # set to False if don't want to parallelize
+if PARALLELIZE:
     random.seed(10)
-    results_n_25 = parallel_ranggraph(n = 25)
-    data_25 = count_data(n = 25,
+    results_n_25 = identification.parallel_rand_graphs_exp(n = 25)
+    data_25 = identification.Countdata(n = 25,
                          reps = 5000,
                          counts = results_n_25,
-                         ran = 10)
-    save_object(data_25)
-    
-    
+                         ran = 100)
+    identification.save_object(data_25)
+
     random.seed(10)
-    results_n_50 = parallel_ranggraph(n = 50)
-    data_50 = count_data(n = 50,
+    results_n_50 = identification.parallel_rand_graphs_exp(n = 50)
+    data_50 = identification.Countdata(n = 50,
                          reps = 5000,
                          counts = results_n_50,
-                         ran = 10)
-    save_object(data_50)
+                         ran = 100)
+    identification.save_object(data_50)
 else:
     random.seed(10)
-    results_n_25 = rangraphsimul(n = 25)
-    data_25 = count_data(n = 25,
+    results_n_25 = identification.rand_graphs_exp(n = 25,
+                                   ran = 50,
+                                   rep = 50)
+    data_25 = identification.Countdata(n = 25,
                          reps = 5000,
                          counts = results_n_25,
-                         ran = 10)
-    
+                         ran = 100)
+
     random.seed(10)
-    results_n_50 = rangraphsimul(n = 50)
-    data_50 = count_data(n = 50,
+    results_n_50 = identification.rand_graphs_exp(n = 50)
+    data_50 = identification.Countdata(n = 50,
                          reps = 5000,
                          counts = results_n_50,
                          ran = 10)
 
 
-##If want to directly load the data uncomment the line below
-# data_25 = load_object("Data/id_count-n_ 25 -medg_ 250-rep_5000.pickle")
-# data_50 = load_object("Data/id_count-n_ 50 -medg_ 500-rep_5000.pickle")
+##If want to directly load the data uncomment the lines below
+# data_25 = identification.load_object("Data/id_count-n_ 25 -medg_ 250-rep_5000.pickle")
+# data_50 = identification.load_object("Data/id_count-n_ 50 -medg_ 500-rep_5000.pickle")
 
-prod_plot_mult_plots(files = [data_25, data_50],
+identification.prod_plot_mult_plots(files = [data_25, data_50],
                      label_fontsize=17,
-                     subtitle_fontsize=25,
-                     legend_fontsize=18) # reproduce Fig. 10
+                     subtitle_fontsize=25) # reproduce Fig. 10 of the paper
--- Causal_Effect_Identification/identification.py
+++ Causal_Effect_Identification/identification.py
@@ -1,3 +1,7 @@
+"""
+Functions for identifying the causal effect vector in a causal graph.
+"""
+
 import random
 import pickle
 import os
@@ -12,36 +16,36 @@ from scipy.sparse.csgraph import maximum_flow
 warnings.filterwarnings("ignore", category=FutureWarning, message="adjacency_matrix will return a scipy.sparse array instead of a matrix in Networkx 3.0.")
 
 
-def find_Rv(g_dir, g_bid, v):
+def find_rv(g_dir, g_bid, v):
     """
     Find the removable ancestors of a node v
     Input:
     - g_dir: Directed graph (networkx.DiGraph) representing the original graph.
     - g_bid: Bidirectional graph (networkx.Graph) representing the original graph.
-    - v: Vertex for which Rv needs to be calculated.
+    - v: Vertex for which rv needs to be calculated.
 
     Output:
-    - Rv: List of vertices that form Rv based on the algorithm.
+    - rv: List of vertices that form rv based on the algorithm.
     """
     # Find the ancestors of vertex v in the directed graph
-    Av = nx.ancestors(g_dir, v)
-    Rv = []
-    
-    # Create a set of neighbors of v in the bidirectional graph
-    Sv = {n for n in g_bid.neighbors(v)}
-    Sv.add(v)  # Include v itself in the set
-    
-    # Iterate over the ancestors in Av
-    for i, u in enumerate(Av):
-        # Create a set of neighbors of u in the bidirectional graph
-        Su = {n for n in g_bid.neighbors(u)}
-        Su.add(u)  # Include u itself in the set
-        
-        # Check if Su is not a subset of Sv, and if not, add u to Rv
-        if not Su.issubset(Sv):
-            Rv.append(u)
+    av = nx.ancestors(g_dir, v)
+    rv = []
 
-    return Rv
+    # Create a set of neighbors of v in the bidirectional graph
+    sv = set(g_bid.neighbors(v))
+    sv.add(v)  # Include v itself in the set
+
+    # Iterate over the ancestors in av
+    for _, u in enumerate(av):
+        # Create a set of neighbors of u in the bidirectional graph
+        su = set(g_bid.neighbors(u))
+        su.add(u)  # Include u itself in the set
+
+        # Check if Su is not a subset of Sv, and if not, add u to rv
+        if not su.issubset(sv):
+            rv.append(u)
+
+    return rv
 
 
 
@@ -71,244 +75,201 @@ def augment_graph(adj_matrix, capacities):
 
 
 
-def check_criterion(g_dir, g_bid, v, Q):
+def check_criterion(g_dir, g_bid, v, q):
     """
-    Check identifiability of the causal effect vector from Q to v
+    Check identifiability of the causal effect vector from q to v
     Input:
     - g_dir: Directed graph (networkx.DiGraph) representing the original graph.
     - g_bid: Bidirectional graph (networkx.Graph) representing the original graph.
     - v: Vertex for which the criterion is checked.
-    - Q: A set of vertices to be checked against the parent set of v.
+    - q: A set of vertices to be checked against the parent set of v.
 
     Output:
     - True if the criterion is satisfied, False otherwise.
     """
-    
     p = len(g_dir.nodes())
-    
-    # Find the parent set of vertex v in the directed graph
-    pav = {n for n in g_dir.predecessors(v)}
-    
-    # Check if Q is not a subset of pav, and if not, raise a ValueError
-    if not Q.issubset(pav):
-        Q = Q & pav
-    
-    if len(Q)==0:
-        return True
-    
-    # Find the ancestors of v in the directed graph
-    Av = nx.ancestors(g_dir, v)
-    
-    # Find the set Rv using the find_Rv function
-    Rv = find_Rv(g_dir, g_bid, v)
-    
-    # Create the adjacency matrix of g_dir
-    Adj = nx.adjacency_matrix(g_dir)
-    
-    # Calculate the max-flow values for Q and pav using the maxflow_Q function
-    val_Q = maxflow_Q(Adj, Av, Rv, v, Q, pav,p)
-    val_pa = maxflow_Q(Adj, Av, Rv, v, pav.difference(Q), pav,p)
-    
-    # Calculate the difference in max-flow values and check against the size of Q
-    val = val_Q - val_pa
-    if val == len(Q):
-        return True
-    else:
-        return False
 
-def maxflow_Q(Adj, Av, Rv, v, Q, pav,p):
+    # Find the parent set of vertex v in the directed graph
+    pav = set(g_dir.predecessors(v))
+
+    # Check if q is not a subset of pav, and if not, add it to the parent set
+    if not q.issubset(pav):
+        q = q & pav
+
+    if len(q) == 0:
+        return True
+
+    # Find the ancestors of v in the directed graph
+    av = nx.ancestors(g_dir, v)
+
+    # Find the set rv using the find_rv function
+    rv = find_rv(g_dir, g_bid, v)
+
+    # Create the adjacency matrix of g_dir
+    adj = nx.adjacency_matrix(g_dir)
+
+    # Calculate the max-flow values for q and pav using the maxflow_q function
+    val_q = maxflow_q(adj, av, rv, v, q, p)
+    val_pa = maxflow_q(adj, av, rv, v, pav.difference(q),p)
+    # Calculate the difference in max-flow values and check against the size of q
+    val = val_q - val_pa
+    return val == len(q)
+
+def maxflow_q(adj, av, rv, v, q, p):
     """
     Calculate max flow based on given parameters
     Input:
-    - Adj: The adjacency matrix of the augmented graph.
-    - Av: Set of ancestors of vertex v in the directed graph.
-    - Rv: List of vertices forming Rv for vertex v.
+    - adj: The adjacency matrix of the augmented graph.
+    - av: Set of ancestors of vertex v in the directed graph.
+    - rv: List of vertices forming rv for vertex v.
     - v: The source vertex.
-    - Q: The set of vertices forming the sink set.
+    - q: The set of vertices forming the sink set.
     - pav: The parent set of vertex v in the directed graph.
 
     Output:
     - Maximum flow value for the given parameters.
     """
     newpar = np.zeros(p)
-    newpar[list(Q)] = 1
-    Adj[:, v] = newpar
-    Adj = Adj.todense()
-    
-    schil = np.zeros(p)
-    schil[Rv] = 1
-    
-    A = np.insert(Adj, 0, schil, 0)
-    A = np.insert(A, 0, np.zeros(p + 1), 1)
+    newpar[list(q)] = 1
+    adj[:, v] = newpar
+    adj = adj.todense()
 
-    Av = [int(x) + 1 for x in Av]
-    Rv = [int(x) + 1 for x in Rv]
-    Q = [int(x) + 1 for x in Q]
+    schil = np.zeros(p)
+    schil[rv] = 1
+
+    a = np.insert(adj, 0, schil, 0)
+    a = np.insert(a, 0, np.zeros(p + 1), 1)
+
+    av = [int(x) + 1 for x in av]
+    rv = [int(x) + 1 for x in rv]
+    q = [int(x) + 1 for x in q]
     v = v + 1
 
-    mf_vert = np.insert(Av, 0, [0], 0)
-    mf_vert = np.insert(mf_vert, len(Av), [v], 0)
-    
-    A = A * (len(Q) + 1)
-    A = A[mf_vert,:][:,mf_vert]
-    
-    mf_cap = np.ones(len(mf_vert) + 1)
-    mf_cap[0] = (len(Q) + 1)
-    mf_cap[len(mf_cap) - 1] = (len(Q) + 1)
-    Aug = augment_graph(A, mf_cap)
+    mf_vert = np.insert(av, 0, [0], 0)
+    mf_vert = np.insert(mf_vert, len(av), [v], 0)
 
-    graph = csr_matrix(Aug)
-    val = maximum_flow(graph, 0,  Aug.shape[0]-1).flow_value
-    
+    a = a * (len(q) + 1)
+    a = a[mf_vert,:][:,mf_vert]
+
+    mf_cap = np.ones(len(mf_vert) + 1)
+    mf_cap[0] = (len(q) + 1)
+    mf_cap[len(mf_cap) - 1] = (len(q) + 1)
+    aug = augment_graph(a, mf_cap)
+
+    graph = csr_matrix(aug)
+    val = maximum_flow(graph, 0,  aug.shape[0]-1).flow_value
+
     return val
 
-def wholematrix_crit(g_dir,g_bid):
+def wholematrix_criterion(g_dir,g_bid):
     """
     Checks the identifiability criterion for the entire matrix of the causal graph.
-    
+
     Args:
     - g_dir: Directed graph (networkx.DiGraph) representing the original graph.
     - g_bid: Bidirectional graph (networkx.Graph) representing the original graph.
-    
+
     Returns:
     - True if the entire matrix satisfies the identifiability criterion, False otherwise.
     """
     for v in range(len(g_dir.nodes())):
-        if check_criterion(g_dir,g_bid,v,{n for n in g_dir.predecessors(v)}) == False:
+        if not check_criterion(g_dir,g_bid,v,set(g_dir.predecessors(v))):
             return False
     return True
 
 
-def ranmixdigraph(n = 25, m = 50):
+def rand_graphs_sample(n = 25, m = 50):
     """
     Generates a random ADMG.
-    
+
     Args:
     - n: Number of nodes in the graph. Default is 25.
     - m: Number of edges in the graph. Default is 50.
-    
+
     Returns:
     - Directed and bidirectional graphs representing the original graph.
     """
-    m_d = random.randint(0,m)
+    m_d = random.randint(0,min(m, n*(n-1)/2))
     g_dir = nx.gnm_random_graph(n, m_d, directed=False)
-    Adir = nx.adjacency_matrix(g_dir)
+    a_dir = nx.adjacency_matrix(g_dir)
     for i in range(n):
         for j in range(i):
-            Adir[i,j] = 0
-    g_dir=nx.DiGraph(Adir.todense())
-    
+            a_dir[i,j] = 0
+    g_dir = nx.DiGraph(a_dir.todense())
+
     m_b = m-m_d
     g_bid = nx.gnm_random_graph(n, m_b, directed=False)
     return g_dir, g_bid
 
-def rangraphsimul(n = 25, ran = 10, rep = 5000):
+def rand_graphs_exp(n = 25, ran = 100, rep = 100):
     """
     Simulates random graphs and calculates the proportion of identifiable ADMGs.
-    
+
     Args:
     - n: Number of nodes in the graph. Default is 25.
-    - ran: Number of iterations. At each iterations the number of edges is the graph 
+    - ran: Number of iterations. At each iterations the number of edges is the graph
     increases of a factor of n.
     - rep: Number of repetitions per iteration. Default is 5000.
-    
+
     Returns:
     - Array containing the proportion of identifiable ADMGs for each iteration.
     """
-    per=np.zeros(ran)
-    for k in range(ran):
-        m = n*(k+1)
+    per = np.zeros(ran)
+    m_max = n*(n-1)
+    for i, k in enumerate(np.linspace(0.01, 0.99, ran)):
+        m = int(m_max*k)
         t = 0
-        for i in range(rep):
-            g_dir, g_bid = ranmixdigraph(n, m)
-            if wholematrix_crit(g_dir,g_bid)==True:
-                t = t+1
-        per[k]=t/rep
+        for _ in range(rep):
+            g_dir, g_bid = rand_graphs_sample(n, m)
+            t += wholematrix_criterion(g_dir,g_bid)
+        per[i] = t/rep
     return per
 
-def rangraphsimul_par(n, ran = 10, rep = 625):
+def rand_graphs_exp_par(n, ran = 100, rep = 625):
     """
     Simulates random graphs in parallel and calculates tthe proportion of identifiable ADMGs.
-    
+
     Args:
     - n: Number of nodes in the graph.
-    - ran: Number of iterations. At each iterations the number of edges is the graph 
+    - ran: Number of iterations. At each iterations the number of edges is the graph
     increases of a factor of n.
     - rep: Number of repetitions per iteration. Default is 625.
-    
+
     Returns:
     - Array containing the count of identifiable ADMGs for each iteration.
     """
-    counts=np.zeros(ran)
-    for k in range(ran):
-        m = n*(k+1)
+    counts = np.zeros(ran)
+    m_max = n*(n-1)
+    for i, m in enumerate(np.linspace(0.01, 0.99, ran)*m_max):
         t = 0
-        for i in range(rep):
-            g_dir, g_bid = ranmixdigraph(n, m)
-            if wholematrix_crit(g_dir,g_bid)==True:
-                t = t+1
-        counts[k]=t
+        for _ in range(rep):
+            g_dir, g_bid = rand_graphs_sample(n, int(m))
+            t += wholematrix_criterion(g_dir,g_bid)
+        counts[i] = t
     return counts
 
-def parallel_ranggraph(n):
+def parallel_rand_graphs_exp(n):
     """
     Executes random graph simulations in parallel.
-    
+
     Args:
     - n: Number of nodes in the graph.
-    
+
     Returns:
     - Sum of results from parallel simulations.
     """
     pool = mp.Pool(8)
-    nlist = np.floor(np.ones(8)*n)
-    
-    results = pool.map(rangraphsimul_par, [int(n) for n in nlist])
-    
-    # Step 3: Don't forget to close
-    pool.close()    
-    
+
+    results = pool.map(rand_graphs_exp_par, [n]*8)
+
+    pool.close()
+
     return sum(results)
-    
-def prod_plot(counts=np.zeros(10),n=25, ran=10, reps=8000, w=22):
-    """
-    Produces a bar plot showing the proportion of identifiable and non-identifiable ADMGs.
-
-    Args:
-    - counts: Array containing the counts of identifiable ADMGs for each iteration.
-    - n: Number of nodes in the graph. Default is 25.
-    - ran: Number of iterations. Default is 10.
-    - reps: Number of repetitions per iteration. Default is 8000.
-    - w: Width of the bars. Default is 22.
-    """
-    type_counts = {
-        'Non-Idenitifiable': reps-counts,
-        'Identifiable': counts,
-        }
-    nedges = [n*(m+1) for m in range(ran)]
-
-    width = w # the width of the bars: can also be len(x) sequence
-
-
-    fig, ax = plt.subplots()
-    bottom = np.zeros(10)
-
-    for t,t_count in type_counts.items():
-        p = ax.bar(nedges, t_count, width, label=t, bottom=bottom)
-        bottom += t_count
-
-        ax.bar_label(p, label_type='center')
-
-    # ax.set_title('Proportion of Identifiable ADMGs with {} nodes'.format(n))
-    ax.set_xlabel('Number of Edges')
-    ax.legend()
-
-    plt.show()
 
 def save_object(obj):
     """
     Saves the object to a pickle file.
-    
+
     Args:
     - obj: Object to be saved.
     """
@@ -321,10 +282,10 @@ def save_object(obj):
 def load_object(filename):
     """
     Loads the object from a pickle file.
-    
+
     Args:
     - filename: Name of the pickle file.
-    
+
     Returns:
     - Loaded object.
     """
@@ -335,10 +296,10 @@ def load_object(filename):
         print("Error during unpickling object (Possibly unsupported):", ex)
 
 
-class count_data:
+class Countdata:
     """
      Represents data containing counts of identifiable ADMGs.
-    
+
      Attributes:
      - n: Number of nodes in the graph.
      - rep: Number of repetitions per iteration.
@@ -351,87 +312,50 @@ class count_data:
         self.rep = reps
         self.count = counts
         self.edges = [n*(m+1) for m in range(ran)]
-        self.head = "Data/id_count-n_ {} -medg_ {}-rep_{}".format(n, n*ran, reps)
-        
-class minim_data:
-    """
-    Represents minimal data.
-    
-    Attributes:
-    - df: Dataframe containing the data.
-    - head: Head of the data.
-    """
-    def __init__(self, data, head):
-        self.df = data
-        self.head = head
-    
+        self.head = f"Data/id_count-n_ {n} -medg_ {n*ran}-rep_{reps}"
+
 def prod_plot_mult_plots(files = None,
                          files_dir = None,
-                         title_fontsize=20,
-                         label_fontsize=13, 
-                         subtitle_fontsize=20,
-                         legend_fontsize=18,
-                         inner_fontsize=11):
+                         label_fontsize=17,
+                         subtitle_fontsize=25,
+                         legend_fontsize=18):
     """
     Produces multiple bar plots showing the proportion of identifiable and non-identifiable ADMGs.
-    
+
     Args:
-    - files (list): A list of count_data objects containing data for plotting. If None, files_dir must be provided.
-    - files_dir (str): Directory path containing count_data objects. Default is None. 
+    - files (list): A list of Countdata objects containing data for plotting. If None, files_dir must be provided.
+    - files_dir (str): Directory path containing Countdata objects. Default is None.
     Either the file list or the directory must be provided.
-    - title_fontsize: Font size for the main title. Default is 16.
-    - label_fontsize: Font size for the axis labels. Default is 13.
-    - subtitle_fontsize: Font size for the subplot titles. Default is 20.
-    - legend_fontsize: Font size for the legend. Default is 13.
-    - inner_fontsize: Font size for the bar labels. Default is 11.
+    - label_fontsize: Font size for the axis labels. Default is 17.
+    - subtitle_fontsize: Font size for the subplot titles. Default is 25.
+    - legend_fontsize: Font size for the legend. Default is 18.
     """
-    
-    fig, axes = plt.subplots(1, 2, figsize=(12, 8), sharey=True)  
-    ran = 10
-    
+
     if files is None:
         if files_dir is None:
             raise ValueError('Specify location for the files, or enter file list.')
-        else:
-            file_names = [f for f in os.listdir(files_dir) if not os.path.isfile(f) and f[0] == "i"]
-            files = []
-            for file_name in file_names:
-                files = [(load_object(files_dir + file_name))] + files                
+        file_names = [f for f in os.listdir(files_dir) if not os.path.isfile(f) and f[0] == "i"]
+        files = []
+        for file_name in file_names:
+            files = [(load_object(files_dir + file_name))] + files
+
+    fig, axs = plt.subplots(1, len(files), figsize=(12, 8), sharey=True)
 
     for i, f in enumerate(files):
-        counts = f.count
+        counts = np.concatenate(([0], f.rep - f.count, [f.rep]))
         n = f.n
-        reps = f.rep
-        if n == 25:
-            w = 22
-        else:
-            w = 44
-        type_counts = {
-            'Non-Identifiable': reps - counts,
-            'Identifiable': counts,
-        }
-        nedges = [n * (m + 1) for m in range(ran)]
+        x_axis = np.concatenate(([0], np.linspace(0.01, 0.99, len(f.count)), [1]))
 
-        width = w
+        ax = axs[i]
+        # Plot results
+        ax.plot(x_axis, counts, color='gray', linewidth=2)
+        ax.fill_between(x_axis, counts, label = 'Non-Identifiable', color='black', alpha=0.4)
+        ax.fill_between(x_axis, counts, f.rep, label = 'Identifiable', color='gray', alpha=0.3)
 
-        ax = axes[i]
-        
-        bottom = np.zeros(10)
-
-        for t, t_count in type_counts.items():
-            color = 'black' if t == 'Non-Identifiable' else 'grey'
-            alpha =  0.4 if t == 'Non-Identifiable' else 0.3
-            p = ax.bar(nedges, t_count, width, label=t, bottom=bottom, color=color, edgecolor='black', alpha = alpha)
-
-            bottom += t_count
-
-            # ax.bar_label(p, label_type='center', fontsize=inner_fontsize, color='black')
-
-        ax.set_title('p = {}'.format(n), fontsize=subtitle_fontsize)
-        ax.set_xlabel('Number of Edges', fontsize=label_fontsize)
+        ax.set_title(f'p = {n}', fontsize=subtitle_fontsize)
+        ax.set_xlabel('Density', fontsize=label_fontsize)
         ax.set_facecolor('white')  # Set background color
-        
-        # Adjusting tick label sizes
+
         ax.tick_params(axis='both', which='major', labelsize=label_fontsize)
         ax.spines['top'].set_color('black')
         ax.spines['right'].set_color('black')
@@ -444,7 +368,7 @@ def prod_plot_mult_plots(files = None,
     handles.reverse()  # Reverse the handles list
     labels.reverse()   # Reverse the labels list
 
-    fig.legend(handles, 
+    fig.legend(handles,
                labels,
                loc='lower center',
                bbox_to_anchor=(0.5, -0.05),
